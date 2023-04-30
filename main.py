5e7fb07e19dadede32179233d54145ffd8deaa01
keval121 <68755073+keval121@users.noreply.github.com>
2023-04-30 06:18:51 -0400

    Update main.py
    Added function: total_goals() and updated the growth_in_value function because I misinterpreted the data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from operator import itemgetter
 
 dataframes = []
 files = ['appearances.csv', 'club_games.csv', 'clubs.csv', 'competitions.csv',
@@ -12,6 +13,137 @@ def file_func():
         dataframes.append(df)
     print('Files processed')
 
+def growth_in_value():
+    df = dataframes[6]
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df['year'] = df['datetime'].dt.year
+    avg_list = []
+    count_list = []
+    g = df.groupby('year')
+    for y, y_df in g:
+        f = y_df.market_value_in_eur
+        avg_list.append(f.mean() / 1000000)
+        count_list.append(f.count())
+    # Plotting the line graph
+    plt.plot(g.groups.keys(), avg_list, 'o-')
+    plt.xlabel('Year')
+    plt.ylabel('Average Market Value (Millions of Euros)')
+    plt.title('Change in Market Values of Players over Time')
+    plt.show()
+
+    # Plotting the line graph
+    plt.plot(g.groups.keys(), count_list, 'o-')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Entries')
+    plt.title('Distribution of Number of Entries per Year')
+    plt.show()
+
+    # As my comparision compare avg values over the years, more of the
+    # recent years have higher number of entries, the data is skewed slightly
+    # Therefore for comaprison, this code is taken from David Coxon as credited
+
+    player_valuations_df = df[(df.year > 2004) & (df.year < 2023)]
+    plt.figure(figsize=(10, 5))
+    plt.scatter(player_valuations_df['datetime'], y=player_valuations_df['market_value_in_eur'] / 1000000, c='b',
+                alpha=0.15)
+    plt.xlabel('Date');
+    plt.ylabel('Valuation in million euros')
+    plt.title('Player valuations over time')
+    plt.show()
+
+
+def value_over_time():
+    # load the dataset and create year column
+    df = dataframes[6]
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df['year'] = df['datetime'].dt.year
+
+    # create list of quarters and dataframes for each quarter
+    quarters = [(2003, 2007), (2008, 2012), (2013, 2017), (2018, 2023)]
+    quarter_dfs = []
+    for start_year, end_year in quarters:
+        quarter_dfs.append(df[(df['year'] >= start_year) & (df['year'] <= end_year)])
+    value_totals = []
+
+    # calculate total transfer fees for each quarter and convert to billions of euros
+    for dfs in quarter_dfs:
+        total = dfs['market_value_in_eur'].sum(skipna = True)
+        total /= 1000000000
+        value_totals.append(total)
+
+    # plot the bar graph for the transfer fees for each quarter
+    positions1 = np.arange(len(quarters))
+    plt.bar(positions1,value_totals, width=0.5, color='r')
+    plt.xticks(positions1, ['2003-2007', '2008-2012', '2013-2017', '2018-2023'])
+    plt.xlabel('5 Year Periods')
+    plt.ylabel('Total Market values (in billions of euros)')
+    plt.title('Market values in 5 year periods from 2003-2023')
+    plt.show()
+
+    # project future transfer fees using a non-linear regression model
+    # add the projected data to the original data
+    value_totals_f = list(value_totals)
+    value_totals_f.extend([743.78289323, 842.4144779])
+    quarters_f = list(quarters)
+    quarters_f.extend([(2023, 2027), (2028, 2032)])
+
+    # plot the bar graph for the projected transfer fees
+    positions2 = np.arange(len(quarters_f))
+    plt.bar(positions2,value_totals_f, width=0.5, color='b')
+    plt.xticks(positions2, ['2003-2007', '2008-2012', '2013-2017', '2018-2023', '2023-2027', '2028-2032'])
+    plt.xlabel('5 Year Periods')
+    plt.ylabel('Total Market values (in billions of euros)')
+    plt.title('Market Values projections using Non-Linear regression : logistics Model')
+    plt.show()
+
+#def value_over_time_by_position():
+
+def total_goals():
+    df = dataframes[5]
+    h_game = df.groupby('home_club_id')
+    a_game = df.groupby('away_club_id')
+
+    h_goals = {}
+    a_goals = {}
+
+    for id, group in h_game:
+        h_goals[id] = group['home_club_goals'].sum()
+    for name, group in a_game:
+        a_goals[id] = group['away_club_goals'].sum()
+
+    total_goals = {}
+    for name in set(h_goals.keys()) | set(a_goals.keys()):
+        total_goals[name] = h_goals.get(name, 0) + a_goals.get(name, 0)
+    # print(total_goals)
+
+    res = dict(sorted(total_goals.items(), key=itemgetter(1), reverse=True)[:10])
+    most_goals = {}
+    for k, v in res.items():
+        d = dataframes[2][dataframes[2].club_id == k]
+        most_goals[d['name'].iloc[0]] = v
+    x_data = list(most_goals.keys())
+    y_data = list(most_goals.values())
+
+    # Create the bar chart
+    plt.figure(figsize=(22, 8))
+    plt.bar(x_data, y_data)
+
+    # Set the title and axis labels
+    plt.title("Top 10 clubs with most goals over the last 10 years")
+    plt.xlabel("Club name")
+    plt.ylabel("Total goals")
+
+    # Show the plot
+    plt.show()
+
+
+file_func()
+growth_in_value()
+value_over_time()
+total_goals()
+
+
+'''
 def growth_in_value():
     df = dataframes[7]
     # Define a list of league codes
@@ -59,54 +191,4 @@ def growth_in_value():
 
     # Show the plot
     plt.show()
-
-         
-def transfer_over_time():
-    # load the dataset and create year column
-    df = dataframes[6]
-    df['datetime'] = pd.to_datetime(df['datetime'])
-    df['year'] = df['datetime'].dt.year
-
-    # create list of quarters and dataframes for each quarter
-    quarters = [(2003, 2007), (2008, 2012), (2013, 2017), (2018, 2023)]
-    quarter_dfs = []
-    for start_year, end_year in quarters:
-        quarter_dfs.append(df[(df['year'] >= start_year) & (df['year'] <= end_year)])
-    transfer_totals = []
-
-    # calculate total transfer fees for each quarter and convert to billions of euros
-    for dfs in quarter_dfs:
-        total = dfs['market_value_in_eur'].sum(skipna = True)
-        total /= 1000000000
-        transfer_totals.append(total)
-
-    # plot the bar graph for the transfer fees for each quarter
-    positions1 = np.arange(len(quarters))
-    plt.bar(positions1,transfer_totals, width=0.5, color='r')
-    plt.xticks(positions1, ['2003-2007', '2008-2012', '2013-2017', '2018-2023'])
-    plt.xlabel('5 Year Periods')
-    plt.ylabel('Total fees (in billions of euros)')
-    plt.title('Transfer fees in 5 year periods from 2003-2023')
-    plt.show()
-
-    # project future transfer fees using a non-linear regression model
-    # add the projected data to the original data
-    transfer_totals_f = list(transfer_totals)
-    transfer_totals_f.extend([743.78289323, 842.4144779])
-    quarters_f = list(quarters)
-    quarters_f.extend([(2023, 2027), (2028, 2032)])
-
-    # plot the bar graph for the projected transfer fees
-    positions2 = np.arange(len(quarters_f))
-    plt.bar(positions2,transfer_totals_f, width=0.5, color='b')
-    plt.xticks(positions2, ['2003-2007', '2008-2012', '2013-2017', '2018-2023', '2023-2027', '2028-2032'])
-    plt.xlabel('5 Year Periods')
-    plt.ylabel('Total fees (in billions of euros)')
-    plt.title('Transfer fees projections using Non-Linear regression : logistics Model')
-    plt.show()
-
-    
-
-file_func()
-growth_in_value()
-transfer_over_time()
+'''
